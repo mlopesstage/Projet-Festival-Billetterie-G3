1943d0e30272f892b616c46de1313bba5812c467
ychantreau <ychantreau@btssio-VirtualBox>
2017-12-07 15:15:41 +0100

    Commit pour réparer quelques erreurs

--- src/modele/dao/DaoGroupe.java
+++ src/modele/dao/DaoGroupe.java
@@ -12,54 +12,52 @@ import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import modele.metier.Representation;
+import modele.metier.Groupe;
 
 /**
  *
  * @author ychantreau
  */
-public class DaoRepresentation {
+public class DaoGroupe {
     /**
      * Extraction d'une representation, sur son identifiant
-     * @param idRepresentation identifiant
-     * @return objet Representation
+     * @param idGroupe identifiant
+     * @return objet Groupe
      * @throws SQLException 
-     */
-    public static Representation selectOne(int idRepresentation) throws SQLException {
-        Representation uneRepresentation = null;
+     *
+    public static Groupe selectOne(int idGroupe) throws SQLException {
+        Groupe unGroupe = null;
         ResultSet rs;
         PreparedStatement pstmt;
         Jdbc jdbc = Jdbc.getInstance();
         // préparer la requête
-        String requete = "SELECT * FROM Representation WHERE ID= ?";
+        String requete = "SELECT * FROM Groupe WHERE ID= ?";
         pstmt = jdbc.getConnexion().prepareStatement(requete);
-        pstmt.setInt(1, idRepresentation);
+        pstmt.setInt(1, idGroupe);
         rs = pstmt.executeQuery();
         if (rs.next()) {
             int id = rs.getInt("id");
             int idLieu = rs.getInt("idLieu");
-            String idGroupe = rs.getString("idGroupe");
             Date dateRep= rs.getDate("dateRep");
             Time heureDebut = rs.getTime("heureDebut");
             Time heureFin = rs.getTime("heureFin");
-            uneRepresentation = new Representation(id, idLieu, idGroupe, dateRep,heureDebut,heureFin);
+            //uneGroupe = new Groupe(id, idLieu, idGroupe, dateRep,heureDebut,heureFin);
         }
-        return uneRepresentation;
+        //return uneGroupe;
     }
-
     /**
      * Extraction de toutes les representations
      * @return collection d'representations
      * @throws SQLException 
      */
-    public static List<Representation> selectAll() throws SQLException {
-        List<Representation> lesRepresentations = new ArrayList<Representation>();
-        Representation uneRepresentation;
+    public static List<Groupe> selectAll() throws SQLException {
+        List<Groupe> lesGroupes = new ArrayList<Groupe>();
+        Groupe uneGroupe;
         ResultSet rs;
         PreparedStatement pstmt;
         Jdbc jdbc = Jdbc.getInstance();
         // préparer la requête
-        String requete = "SELECT * FROM Representation";
+        String requete = "SELECT * FROM Groupe";
         pstmt = jdbc.getConnexion().prepareStatement(requete);
         rs = pstmt.executeQuery();
         while (rs.next()) {
@@ -69,10 +67,10 @@ public class DaoRepresentation {
             Date dateRep= rs.getDate("dateRep");
             Time heureDebut = rs.getTime("heureDebut");
             Time heureFin = rs.getTime("heureFin");
-            uneRepresentation = new Representation(id, idLieu, idGroupe, dateRep,heureDebut,heureFin);
-            lesRepresentations.add(uneRepresentation);
+           // uneGroupe = new Groupe(id, idLieu, idGroupe, dateRep,heureDebut,heureFin);
+            //lesGroupes.add(uneGroupe);
         }
-        return lesRepresentations;
+        return lesGroupes;
     }
     
         /**
@@ -81,14 +79,14 @@ public class DaoRepresentation {
      * @return collection d'representations
      * @throws SQLException 
      *
-    public static List<Representation> selectAllByVille(String extraitNomVille) throws SQLException {
-        List<Representation> lesRepresentations = new ArrayList<Representation>();
-        Representation uneRepresentation;
+    public static List<Groupe> selectAllByVille(String extraitNomVille) throws SQLException {
+        List<Groupe> lesGroupes = new ArrayList<Groupe>();
+        Groupe uneGroupe;
         ResultSet rs;
         PreparedStatement pstmt;
         Jdbc jdbc = Jdbc.getInstance();
         // préparer la requête
-        String requete = "SELECT * FROM Representation WHERE ville LIKE ?";
+        String requete = "SELECT * FROM Groupe WHERE ville LIKE ?";
         pstmt = jdbc.getConnexion().prepareStatement(requete);
         pstmt.setString(1, "%"+extraitNomVille+"%");
         rs = pstmt.executeQuery();
@@ -97,12 +95,11 @@ public class DaoRepresentation {
             String rue = rs.getString("RUE");
             String cdp = rs.getString("CDP");
             String ville = rs.getString("VILLE");
-            uneRepresentation = new Representation(id, rue, cdp, ville);
-            lesRepresentations.add(uneRepresentation);
+            uneGroupe = new Groupe(id, rue, cdp, ville);
+            lesGroupes.add(uneGroupe);
         }
-        return lesRepresentations;
+        return lesGroupes;
     }    
-
     /**
      * Extraction de toutes les representations, ordonnées
      * @param cleTri 1=>ID ; 2=>VILLE
@@ -110,14 +107,14 @@ public class DaoRepresentation {
      * @return collection de representations
      * @throws SQLException 
      *
-    public static List<Representation> selectAll(int cleTri, int ordreTri) throws SQLException {
-        List<Representation> lesRepresentations = new ArrayList<Representation>();
-        Representation uneRepresentation;
+    public static List<Groupe> selectAll(int cleTri, int ordreTri) throws SQLException {
+        List<Groupe> lesGroupes = new ArrayList<Groupe>();
+        Groupe uneGroupe;
         ResultSet rs;
         PreparedStatement pstmt;
         Jdbc jdbc = Jdbc.getInstance();
         // préparer la requête
-        String requete = "SELECT * FROM Representation";
+        String requete = "SELECT * FROM Groupe";
         switch (cleTri) {
             case 1: // Tri par Id
                 requete += " ORDER BY ID";
@@ -143,53 +140,50 @@ public class DaoRepresentation {
             String rue = rs.getString("RUE");
             String cdp = rs.getString("CDP");
             String ville = rs.getString("VILLE");
-            uneRepresentation = new Representation(id, rue, cdp, ville);
-            lesRepresentations.add(uneRepresentation);
+            uneGroupe = new Groupe(id, rue, cdp, ville);
+            lesGroupes.add(uneGroupe);
         }
-        return lesRepresentations;
+        return lesGroupes;
     }
-
-    public static int insert(int idRepresentation, Representation uneRepresentation) throws SQLException {
+    public static int insert(int idGroupe, Groupe uneGroupe) throws SQLException {
         int nb;
         Jdbc jdbc = Jdbc.getInstance();
         String requete;
         ResultSet rs;
         PreparedStatement pstmt;
-        requete = "INSERT INTO Representation (ID, RUE, CDP , VILLE) VALUES (?, ?, ?, ?)";
+        requete = "INSERT INTO Groupe (ID, RUE, CDP , VILLE) VALUES (?, ?, ?, ?)";
         pstmt = jdbc.getConnexion().prepareStatement(requete);
-        pstmt.setInt(1, idRepresentation);
-        pstmt.setString(2, uneRepresentation.getRue());
-        pstmt.setString(3, uneRepresentation.getCp());
-        pstmt.setString(4, uneRepresentation.getVille());
+        pstmt.setInt(1, idGroupe);
+        pstmt.setString(2, uneGroupe.getRue());
+        pstmt.setString(3, uneGroupe.getCp());
+        pstmt.setString(4, uneGroupe.getVille());
         nb = pstmt.executeUpdate();
         return nb;
     }
-
-    public static int update(int idRepresentation, Representation uneRepresentation) throws SQLException {
+    public static int update(int idGroupe, Groupe uneGroupe) throws SQLException {
         int nb;
         Jdbc jdbc = Jdbc.getInstance();
         String requete;
         ResultSet rs;
         PreparedStatement pstmt;
-        requete = "UPDATE Representation SET RUE = ? , CDP = ? , VILLE = ? WHERE ID = ?";
+        requete = "UPDATE Groupe SET RUE = ? , CDP = ? , VILLE = ? WHERE ID = ?";
         pstmt = jdbc.getConnexion().prepareStatement(requete);
-        pstmt.setString(1, uneRepresentation.getRue());
-        pstmt.setString(2, uneRepresentation.getCp());
-        pstmt.setString(3, uneRepresentation.getVille());
-        pstmt.setInt(4, idRepresentation);
+        pstmt.setString(1, uneGroupe.getRue());
+        pstmt.setString(2, uneGroupe.getCp());
+        pstmt.setString(3, uneGroupe.getVille());
+        pstmt.setInt(4, idGroupe);
         nb = pstmt.executeUpdate();
         return nb;
     }
-
-    public static int delete(int idRepresentation) throws SQLException {
+    public static int delete(int idGroupe) throws SQLException {
         int nb;
         Jdbc jdbc = Jdbc.getInstance();
         String requete;
         ResultSet rs;
         PreparedStatement pstmt;
-        requete = "DELETE  FROM Representation WHERE ID = ?";
+        requete = "DELETE  FROM Groupe WHERE ID = ?";
         pstmt = jdbc.getConnexion().prepareStatement(requete);
-        pstmt.setInt(1, idRepresentation);
+        pstmt.setInt(1, idGroupe);
         nb = pstmt.executeUpdate();
         return nb;
     }*/
--- src/test/modele/dao/TestDaoRepresentation.java
+++ src/test/modele/dao/TestDaoRepresentation.java
@@ -1,14 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package test.modele.dao;
+
+import java.sql.*;
+import java.util.List;
+import modele.dao.DaoRepresentation;
+import modele.dao.Jdbc;
+import modele.metier.Representation;
 
 /**
  *
- * @author ychantreau
+ * @author btssio
  */
 public class TestDaoRepresentation {
-    
+
+    public static void main(String[] args) {
+
+        java.sql.Connection cnx = null;
+
+        try {
+            test0_Connexion();
+            System.out.println("Test0 effectué : connexion\n");
+            test1_SelectUnique(3);
+            System.out.println("Test1 effectué : sélection unique\n");
+            test2_SelectMultiple();
+            System.out.println("Test2 effectué : sélection multiple\n");
+        } catch (ClassNotFoundException e) {
+            System.err.println("Erreur de pilote JDBC : " + e);
+        } catch (SQLException e) {
+            System.err.println("Erreur SQL : " + e);
+        } finally {
+            try {
+                if (cnx != null) {
+                    cnx.close();
+                }
+            } catch (SQLException e) {
+                System.err.println("Erreur de fermeture de la connexion JDBC : " + e);
+            }
+        }
+
+    }
+
+    /**
+     * Vérifie qu'une connexion peut être ouverte sur le SGBD
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    public static void test0_Connexion() throws ClassNotFoundException, SQLException {
+        Jdbc.creer("com.mysql.jdbc.Driver", "jdbc:mysql://", "localhost/", "festival2", "root", "joliverie");
+//        Jdbc.creer("oracle.jdbc.driver.OracleDriver", "jdbc:oracle:thin:", "@localhost:1521:XE", "", "btssio", "btssio");
+        Jdbc.getInstance().connecter();
+        Connection cnx = Jdbc.getInstance().getConnexion();
+    }
+
+    /**
+     * Affiche une adresse d'après son identifiant
+     * @param idRepresentation
+     * @throws SQLException
+     */
+    public static void test1_SelectUnique(int idRepresentation) throws SQLException {
+        Representation cetteRepresentation = DaoRepresentation.selectOne(idRepresentation);
+        System.out.println("Representation d'identifiant : "+ idRepresentation +" : "+cetteRepresentation.toString());
+    }
+
+    /**
+     * Affiche toutes les representations
+     * @throws SQLException
+     */
+    public static void test2_SelectMultiple() throws SQLException {
+        List<Representation> desRepresentations = DaoRepresentation.selectAll();
+        System.out.println("Les representations lues : "+desRepresentations.toString());
+    }
 }
